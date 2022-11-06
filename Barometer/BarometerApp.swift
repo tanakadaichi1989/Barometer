a916187cd3c2ca667703b703f3e03c5d70a9ea74
Daichi Tanaka <tanakadaichi_1989@icloud.com>
2022-11-06 12:12:04 +0900

    Initial Commit

---
+++ Barometer/BarometerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarometerApp.swift
+//  Barometer
+//
+//  Created by 田中大地 on 2022/11/06.
+//
+
+import SwiftUI
+
+@main
+struct BarometerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
